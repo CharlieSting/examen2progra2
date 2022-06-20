7daa87538e3c5e974924e9609a6c1834dc0cdbd6
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2022-06-19 20:01:50 -0600

    primer comit v1

---
+++ A.Electronicos/A.Electronicos/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using A.Electronicos.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace A.Electronicos.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
